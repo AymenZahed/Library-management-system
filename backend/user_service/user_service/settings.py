e786aa1d1f80f72ca6e40db9d24ab94fe8cb939c
Abdelhafidh Amrane <abdelhafidh.amrane@gmail.com>
2025-11-16 10:31:53 +0100

    configuring users db

--- backend/user_service/user_service/settings.py
+++ backend/user_service/user_service/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'user_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'users_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '8001',
     }
 }
 
