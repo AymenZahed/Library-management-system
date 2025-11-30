54107faa72304a8fe1499ac0a0f6417040965c23
Aymen Zahed <aymenzahed62@gmail.com>
2025-11-30 16:48:59 +0100

    change the secret_key

--- backend/books_service/books_service/settings.py
+++ backend/books_service/books_service/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g0(3jcm+a3apy5fmoow%t#%o11it1m%f$#)g-vmk5$@ka$hbn7'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
